507df4ed47bc00d0c26968ac10a193ed5657e423
Sebastian Pietras <me@spietras.dev>
2024-10-19 22:15:01 +0200

    Changed ports

--- src/api/mantis/client.ts
+++ src/api/mantis/client.ts
@@ -7,7 +7,7 @@ const scheme = process.env.DAISY__MANTIS__SCHEME || "http";
 const host = process.env.DAISY__MANTIS__HOST || "localhost";
 const port =
   process.env.DAISY__MANTIS__PORT === undefined
-    ? 33000
+    ? 10800
     : process.env.DAISY__MANTIS__PORT;
 const path = (process.env.DAISY__MANTIS__PATH || "")
   // Ensure path starts with a slash
