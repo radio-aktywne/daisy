c96df4acf985daf6998d04b78ee984f21c12198b
Sebastian Pietras <me@spietras.dev>
2026-03-14 21:06:20 +0100

    Cleaned up code

--- src/app/(root)/(tasks)/(master)/(detail)/tasks/[id]/page.tsx
+++ src/app/(root)/(tasks)/(master)/(detail)/tasks/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { msg } from "@lingui/core/macro";
+import { notFound } from "next/navigation";
 import { connection } from "next/server";
 
 import type {
@@ -8,17 +9,35 @@ import type {
 } from "../../../../../../types";
 import type { Keys } from "./types";
 
+import { isOrpcDefinedError } from "../../../../../../../common/orpc/lib/is-orpc-defined-error";
 import { Metadata } from "../../../../../../../isomorphic/metadata/components/metadata";
 import { createMetadata } from "../../../../../../../server/metadata/lib/create-metadata";
+import { orpcServerSideQueryClient } from "../../../../../../../server/orpc/vars/clients";
+import { getQueryClient } from "../../../../../../../server/query/lib/get-query-client";
 import { TasksIdPageView } from "./page.view";
 import { Schemas } from "./schemas";
 
 async function getTitle({
   pathParameters,
 }: PageMetadataUtilityInput<typeof Schemas.Path, typeof Schemas.Query>) {
-  const { id } = pathParameters;
+  const { queryClient } = getQueryClient();
 
-  return msg({ message: `Task ${id} • daisy` });
+  const task = await (async () => {
+    try {
+      return await queryClient.fetchQuery(
+        orpcServerSideQueryClient.core.getTask.queryOptions({
+          input: { id: pathParameters.id },
+        }),
+      );
+    } catch (error) {
+      if (isOrpcDefinedError(error) && error.code === "NOT_FOUND") notFound();
+      throw error;
+    }
+  })();
+
+  const taskId = task.task.id;
+
+  return msg({ message: `Task ${taskId} • daisy` });
 }
 
 export async function generateMetadata({
